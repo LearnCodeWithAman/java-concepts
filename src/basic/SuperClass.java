03f4a2c590dfe6d0b7d7939ab0b6650f62598687
amanverma01 <amanvrm543@gmail.com>
2024-04-05 14:44:29 +0530

    Updated : Streams

---
+++ src/basic/SuperClass.java
@@ -0,0 +1,33 @@
+package basic;
+
+import java.io.IOException;
+
+//demonstrat Exception Propagation
+
+abstract class SuperClass{
+
+    public SuperClass(){
+    }
+
+    public void method1() throws Exception {
+        System.out.println("SuperClass");
+    }
+
+    public abstract void method2();
+    }
+
+    class ChildClass extends SuperClass {
+    public void method1() throws IOException {
+        System.out.println("ChildClass");
+    }
+
+    @Override
+    public void method2() {
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        ChildClass childClass = new ChildClass();
+        childClass.method1();
+    }
+}
